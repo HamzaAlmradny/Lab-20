1f571bec2f092f0a5ffcf6d0cfcf385c88d2f1b1
Dr. Alsayed <Dr. Alsayed@DESKTOP-APIOASQ>
2025-07-29 21:20:51 +0300

    Add project files.

---
+++ Lab 20/Lab 20.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+
+	float num;
+
+	cout << "Enter the number: ";
+	cin >> num;
+
+
+	if (num < 0) {
+		num = -num;
+	
+}
+
+	cout << "The absolute number is " << num << "\n";
+
+
+
+
+
+
+
+
+
+	return 0;
+}
